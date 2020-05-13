5555c406335d608950e16dfde82d3256e1dfff65
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-05-13 19:27:22 -0300

    atualizando

--- db/schema.rb
+++ db/schema.rb
@@ -15,19 +15,6 @@ ActiveRecord::Schema.define(version: 2020_04_14_002402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "possui_doenca", force: :cascade do |t|
-    t.boolean "nenhuma"
-    t.boolean "idoso"
-    t.boolean "asma"
-    t.boolean "cancer"
-    t.boolean "diabetico"
-    t.boolean "doe_cardio"
-    t.boolean "hipertensao"
-    t.boolean "fumante"
-    t.boolean "gestante"
-    t.boolean "obesidade"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,12 +25,22 @@ ActiveRecord::Schema.define(version: 2020_04_14_002402) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "last_name"
-    t.date "data_nasc"
+    t.string "idade"
     t.string "cep"
     t.string "estado"
     t.string "cidade"
     t.string "bairro"
     t.string "rua"
+    t.boolean "nenhuma"
+    t.boolean "idoso"
+    t.boolean "asma"
+    t.boolean "cancer"
+    t.boolean "diabetico"
+    t.boolean "doe_cardio"
+    t.boolean "hipertensao"
+    t.boolean "fumante"
+    t.boolean "gestante"
+    t.boolean "obesidade"
     t.boolean "possui_crianca"
     t.boolean "possui_risco"
     t.index ["email"], name: "index_users_on_email", unique: true
